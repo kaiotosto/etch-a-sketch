4db3bc8a738835a4c690e0a8cd62f151da387840
Kaio Tosto <kaiotosto@gmail.com>
2023-02-05 12:35:54 -0300

    Add clear button

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const container = document.querySelector(".container");
+const clear = document.querySelector(".clear");
+let gridSize = 16;
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < gridSize * gridSize; i++) {
 	const div = document.createElement("div");
 	div.classList.add("grid");
 	div.style.width = "20px";
@@ -14,3 +16,9 @@ for (const box of grid) {
 		box.style.backgroundColor = "aqua";
 	});
 }
+
+clear.addEventListener("click", () => {
+	for (const box of grid) {
+		box.style.backgroundColor = "black";
+	};
+});
