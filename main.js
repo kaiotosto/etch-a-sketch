ac14f84f2f36d475418c49f63b40db6c8dfd34a4
Kaio Tosto <kaiotosto@gmail.com>
2023-02-05 12:15:36 -0300

    Add mouse over event

--- main.js
+++ main.js
@@ -3,5 +3,14 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
 	const div = document.createElement("div");
 	div.classList.add("grid");
+	div.style.width = "20px";
+	div.style.height = "20px";
 	container.append(div);
+}
+
+let grid = document.querySelectorAll(".grid");
+for (const box of grid) {
+	box.addEventListener("mouseover", () => {
+		box.style.backgroundColor = "aqua";
+	});
 }
